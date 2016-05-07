9eabbeecb35ba1b119a16cdf95a0e36fc9536ce9
alexchetv <alexchetv@gmail.com>
2016-05-07 17:13:36 +0500

    Add caching

--- server.js
+++ server.js
@@ -7,6 +7,28 @@ const fs = require('fs');
 const secret = require('./secret');
 var tg = require('telegram-node-bot')(secret.token);
 var req = require('tiny_request');
+var request = require('request');
+var cradle = require('cradle');
+var db = new (cradle.Connection)().database('telegram');
+
+db.exists(function (err, exists) {
+	if (err) {
+		console.error('Database Error', err);
+	} else if (exists) {
+		console.log('Database Connected.');
+		db.info(
+			function (err, info) {
+				if (err) {
+					console.error('Database Info Error', err);
+				} else {
+					console.log(info);
+				}
+			}
+		)
+	} else {
+		console.error('Database does not exists.');
+	}
+});
 
 tg.router.
 	when(['/start', '/Start'], 'StartController').
@@ -34,7 +56,7 @@ tg.controller('WordsController', ($) => {
 		var query_norm = $.args.replace('_', ' ').replace('/', '');
 		$.sendMessage('Now seeking <b>' + query_norm + '</b> ...', {parse_mode: 'HTML'}, (answer, err) => {
 			if (!err) {
-				console.log('answer', answer);
+				//console.log('answer', answer);
 				seekPhrase($, answer.result, query_norm);
 			} else {
 				console.error('error Send Message', err);
@@ -69,54 +91,90 @@ tg.callbackQueries(($) => {
  }])
  })*/
 
-var show = ($, url, caption)=> {
-	console.log('show');
-	var tempStream = fs.createWriteStream('temp.mp4');
-	req.get({
-			url: url,
-			pipe: tempStream
-		},
-		function (body, response, err) {
-			console.log('sendVideo');
-			$.sendVideo(fs.createReadStream('temp/temp.mp4'), {caption: caption});
-		});
-}
-
-var showVideos = ($, a, n)=> {
-	n = n?n:0;
-	if (n < a.length) {
-		var fileName = 'video/' + a[n]._id + ".mp4";
-		var wstream = fs.createWriteStream(fileName);
-		var self = this;
-		wstream.on('finish', () => {
-			$.sendVideo(fs.createReadStream(fileName), {caption: a[n].caption}, (body, err) => {
-				if (err) {
-					console.error('error Send Video', err);
-				} else {
-					if (body && body.result && body.result.video && body.result.video.file_id) {
-						savedVideo.push({
-							file_id: body.result.video.file_id,
-							url: a[n].url
-						})
+var sendVideoFromAttach = (id, $, a, n, tfid) => {
+	if (tfid) {
+		$.sendVideo(tfid, {caption: a[n].caption}, (body, err) => {
+			if (err || !body || !body.ok) {
+				console.error('error Send TFID', err ? err : body);
+				db.merge(id, {telegram_file_id: null}, function (err, res) {
+					if (err) {
+						console.error('error Delete TFID', err);
+					} else {
+						showVideos($, a, n);
 					}
-					showVideos($, a, n+1);
+				});
+			} else {
+				showVideos($, a, n + 1);
+			}
+		})
+	} else {
+		var readFromAttachStream = db.getAttachment(id, 'video', function (err) {
+			if (err) {
+				console.error('error getAttachment', err);
+			}
+		});
+
+		var fileName = 'temp/' + Math.random().toString(16) + '.mp4';
+		var writeToFileStream = fs.createWriteStream(fileName);
+		writeToFileStream.on('finish', () => {
+
+			$.sendVideo(fs.createReadStream(fileName), {caption: a[n].caption}, (body, err) => {
+				fs.unlink(fileName);
+				if (err || !body || !body.ok) {
+					console.error('error Send Video', err ? err : body);
+				} else {
+					if (body.result && body.result.video && body.result.video.file_id) {
+						db.merge(a[n]._id, {telegram_file_id: body.result.video.file_id}, function (err, res) {
+							if (err) {
+								console.error('error Merge telegram_file_id', err);
+							}
+						});
+					}
+					showVideos($, a, n + 1);
 				}
 			})
 		})
-
-		req.get({
-				url: a[n].url,
-				pipe: wstream
-			},
-			function (body, response, err) {
-				if (err) {
-					console.error('error Load Video', err);
-				}
-			});
+		readFromAttachStream.pipe(writeToFileStream);
 	}
 }
 
-var savedVideo = [];
+var showVideos = ($, a, n)=> {
+	n = n ? n : 0;
+	if (n < a.length) {
+		db.get(a[n]._id, function (err, doc) {
+			if (doc && doc._attachments && doc._attachments.video && doc._attachments.video.stub) {
+				console.log('Doc ', doc);
+				sendVideoFromAttach(a[n]._id, $, a, n, doc.telegram_file_id);
+			} else {
+				console.log('Doc Not Found');
+
+				var writeToAttachStream;
+				db.save(a[n]._id, {
+					text: a[n].caption
+				}, function (err, res) {
+					if (err) {
+						console.error('error Save DocumentToDB', err);
+					} else {
+						var attachmentData = {
+							name: 'video',
+							'Content-Type': 'video/mp4'
+						}
+						writeToAttachStream = db.saveAttachment({id: res.id, rev: res.rev}, attachmentData,
+							function (err, res) {
+								if (err) {
+									console.error('error saveAttachment', err);
+								} else {
+									sendVideoFromAttach(res.id, $, a, n);
+								}
+							}
+						)
+						request(a[n].url).pipe(writeToAttachStream);
+					}
+				});
+			}
+		});
+	}
+}
 
 var seekPhrase = ($, sent_message, q)=> {
 	{
